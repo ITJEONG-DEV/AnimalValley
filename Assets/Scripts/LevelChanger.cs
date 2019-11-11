d349403dd7caaf9a8697f39c1af41eeab3ab4578
americanojoajoa <ford0828@naver.com>
2019-11-11 11:17:21 +0900

    [ADD] Asset
    에셋 몬스터/ 농기구 추가함
     씬 변경 애니메이션  추가함

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("LoadScene");
+        }
+    }
+   
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
