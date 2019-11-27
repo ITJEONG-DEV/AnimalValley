5c03f6baaae55317648d8bc26eb41bed82eb1c15
ITJEONG <derbana1027@gmail.com>
2019-11-27 19:51:34 +0900

    [ADD] save&load, itemList
    1. save 및 load 기능 초기 구현
      [time info] day hour minute
      [story status] status
      [gold info] gold
      [status info] hp cur_hp energe cur_energe
      [item info] { code, number }
      [character info] characterType characterSkin characterEmotion accessory1 accessory2 hand
      [user info] name
      [valley info] valleyName
    
    2. itemList
      아이템 리스트 구현

--- Assets/Scripts/SystemSettings/CharacterSettings/Settings.cs
+++ Assets/Scripts/SystemSettings/CharacterSettings/Settings.cs
@@ -4,29 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum ItemType1
-{
-    Tool, Resource, Facility, Buff
-};
-
-public class Item
-{
-    private string itemCode;
-    private string name;
-    private string itemIcon;
-    private string itemPrefab;
-
-    public Item(string itemCode)
-    {
-        this.itemCode = itemCode;
-        name = Settings.GetItemName(itemCode);
-        itemIcon = "Assets/Item/Icon" + itemCode + ".png";
-        itemPrefab = "Assets/Item/Prefab" + itemCode + ".prefab";
-
-        Debug.Log(this.itemCode + "->" + this.name + " 생성됨" );
-    }
-}
-
 public static class Settings
 {
     private static bool hasItemNamePairData = false;
@@ -46,19 +23,20 @@ public static class Settings
 
         try
         {
-            string path = "Assets/Scripts/Settings/item.data";
+            string path = "Assets/Scripts/SystemSettings/CharacterSettings/item.data";
             FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
             StreamReader streamReader = new StreamReader(fileStream);
 
             string[] lines = streamReader.ReadToEnd().Trim('\n').Split('\n');
-            
-            foreach(string line in lines)
+
+            foreach (string line in lines)
             {
                 string[] data = line.Split(',');
                 itemNamePair.Add(new KeyValuePair<string, string>(data[0], data[1]));
             }
 
-        }catch(Exception exception)
+        }
+        catch (Exception exception)
         {
             Debug.Log(exception.Message);
             Debug.Log("FAILED: ");
@@ -71,7 +49,7 @@ public static class Settings
     public static string GetItemName(string itemCode)
     {
         string name = "";
-        if(!hasItemNamePairData)
+        if (!hasItemNamePairData)
         {
             Debug.Log("아이템 정보를 불러오지 않았음");
             return null;
---
+++ Assets/Scripts/SystemSettings/CharacterSettings/Status.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Status
+{
+    private static int hp = 150;
+    private static int current_hp;
+    private static int energe = 200;
+    private static int current_energe;
+
+    public static string ToString()
+    {
+        return "hp : " + current_hp + "/" + hp + ", energe : " + current_energe + "/" + energe; 
+    }
+
+    public static int MAX_HP
+    {
+        get
+        {
+            return hp;
+        }
+    }
+    public static int CUR_HP
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            hp = value;
+        }
+    }
+
+    public static int MAX_ENERGE
+    {
+        get
+        {
+            return energe;
+        }
+    }
+    public static int CUR_ENERGE
+    {
+        get
+        {
+            return current_energe;
+        }
+        set
+        {
+            current_energe = value;
+        }
+    }
+
+    public static void SetHp(int hp)
+    {
+        Status.hp = hp;
+        Status.current_hp = hp;
+    }
+
+    public static void SetEnerge(int energe)
+    {
+        Status.energe = energe;
+        Status.current_energe = energe;
+    }
+
+    public static void AddHP(int cost)
+    {
+        hp += cost;
+    }
+
+    public static void AddEnerge(int cost)
+    {
+        energe += cost;
+    }
+}
+
