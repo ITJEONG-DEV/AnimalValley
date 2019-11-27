7a5d5e6721ba4dff5d9470259c4dfb3df7b65111
americanojoajoa <ford0828@naver.com>
2019-11-27 22:06:17 +0900

    [ADD] 동물선택창
    랜덤 안되고 하나씩 표정,스킨 선택
    무기 버튼 지움

---
+++ Assets/Scripts/characterSelectionButton/emotionIndex.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emotionIndex : MonoBehaviour
+{
+    public GameObject[] faces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    public void onClick()
+    {
+        if (GameObject.FindGameObjectWithTag("bunny"))
+            faces[0].GetComponent<ExampleSwapMaterials>().onclickEvent();
+        else if (GameObject.FindGameObjectWithTag("cat"))
+            faces[1].GetComponent<ExampleSwapMaterials>().onclickEvent();
+        else if (GameObject.FindGameObjectWithTag("bear") == true)
+            faces[2].GetComponent<ExampleSwapMaterials>().onclickEvent();
+    }
+}
